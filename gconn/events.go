0f53769aa5b4ba6b4e0e4a1c6df2f87a6aec13d7
Burak Sezer <bburaksezer@gmail.com>
2014-07-07 03:59:08 +0300

    event registering, introduce

---
+++ gconn/events.go
@@ -0,0 +1,40 @@
+package gconn
+
+import (
+	"bytes"
+	_ "fmt"
+	"github.com/zond/gotomic"
+)
+
+type Key []byte
+
+type Listener struct {
+	HashTable *gotomic.Hash
+}
+
+func (self Key) HashCode() uint32 {
+	var rval uint32
+	for c := range self {
+		rval = rval + uint32(c)
+	}
+	return rval
+}
+
+func (self Key) Equals(t gotomic.Thing) bool {
+	return bytes.Equal(self, t.(Key))
+}
+
+func (self *Conn) RegisterListener() *Listener {
+	h := gotomic.NewHash()
+	return &Listener{
+		HashTable: h,
+	}
+}
+
+func (self *Listener) Put(k, t gotomic.Thing) {
+	self.HashTable.Put(Key(k.(Key)), t)
+}
+
+func (self *Listener) Get(k gotomic.Thing) (gotomic.Thing, bool) {
+	return self.HashTable.Get(Key(k.(Key)))
+}
